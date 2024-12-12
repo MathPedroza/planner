1ebdbffd2bd466bf2bea07704cb2cd1730d00c6f
Matheus Correia Pedroza <54449360+MathPedroza@users.noreply.github.com>
2024-12-11 22:01:20 -0300

    Update Footer.jsx

--- api-frontend/src/components/Footer.jsx
+++ api-frontend/src/components/Footer.jsx
@@ -1,14 +1,14 @@
-import './Footer.css'
-import React from 'react'
+import './Footer.css';
+import React from 'react';
 
-export default props =>
-
-    <footer div style={{padding: "1px",
-                        position: "relative",
-    }} className="footer">
+const Footer = () => (
+    <footer className="footer">
         <span>
             <br/>
             Desenvolvido pelos alunos de <i className="fa fa-heart text-danger"></i>
-            <strong>Análise e Desenvolvimento Web<span className="text-danger"></span></strong>
+            <strong> Análise e Desenvolvimento Web</strong>
         </span>
     </footer>
+);
+
+export default Footer;
