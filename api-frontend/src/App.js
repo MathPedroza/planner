9760ed6eaa24568ef97deb47e5586cc5d8ccfa2b
Matheus Correia Pedroza <54449360+MathPedroza@users.noreply.github.com>
2024-12-13 16:56:31 -0300

    Update App.js

--- api-frontend/src/App.js
+++ api-frontend/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'; // Importe Navigate
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import './App.css';
@@ -7,26 +7,26 @@ import View from './pages/View';
 import Footer from './components/Footer';
 import Nav from './components/Nav';
 import ViewLembrete from './pages/ViewLembrete';
-import Home from './pages/Home';
+import Home from './pages/home'; 
 import Sobre from './pages/Sobre';
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Nav />
+        <Nav/>
         <ToastContainer position="top-center" />
         <Routes>
-          {/* Definição das rotas */}
-          <Route path="/home" element={<Home />} />
-          <Route path="/sobre" element={<Sobre />} />
+          <Route path="/" element={<Navigate to="/home" />} /> {/* Aplicação inicia já na página Home */}
+          <Route exact path="/home" element={<Home />} />
+          <Route exact path="/sobre" element={<Sobre />} />
           <Route path="/addLembrete" element={<AddLembrete />} />
           <Route path="/update/:id" element={<AddLembrete />} />
           <Route path="/ViewLembrete" element={<ViewLembrete />} />
           <Route path="/view/:id" element={<View />} />
         </Routes>
       </div>
-      <Footer />
+      <Footer/>
     </Router>
   );
 }
