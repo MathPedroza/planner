b9de64b67776d61357075550b491879aa423298f
Matheus Correia Pedroza <54449360+MathPedroza@users.noreply.github.com>
2024-12-17 14:36:42 -0300

    Update Nav.jsx

--- api-frontend/src/components/Nav.jsx
+++ api-frontend/src/components/Nav.jsx
@@ -1,20 +1,31 @@
 import './Nav.css';
 import React from 'react';
-import { Link } from 'react-router-dom';
-import Home from '../pages/home';
-import Sobre from '../pages/Sobre';
+import { Link } from 'react-router-dom'; // Importa React e o componente Link do React Router
 
-export default props =>
+// Componente funcional Nav
+const Nav = () => {
+  return ( 
+    // Área lateral do menu
     <aside className="menu-area">
-        <nav className="menu">
-            <Link to="/home">
-                <i className="fa fa-home"></i> Início
-            </Link>
-            <Link to="/ViewLembrete">
-                <i className="fa fa-users"></i> Lembretes
-            </Link>
-            <Link to="/sobre">
-                <i className="fa fa-users"></i> Sobre
-            </Link>
-        </nav>
+      <nav className="menu">
+        {/* Link para a página inicial */}
+        <Link to="/home">
+          <i className="fa fa-home"></i> Início
+        </Link>
+
+        {/* Link para a página de Lembretes */}
+        <Link to="/ViewLembrete">
+          <i className="fa fa-sticky-note"></i> Lembretes
+        </Link>
+
+        {/* Link para a página Sobre */}
+        <Link to="/sobre">
+          <i className="fa fa-info-circle"></i> Sobre
+        </Link>
+      </nav>
     </aside>
+  );
+};
+
+// Exporta o componente Nav para uso em outros arquivos
+export default Nav;
