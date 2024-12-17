cec0ad83df66d5dd4555d819ccae7423b91cb693
Matheus Correia Pedroza <54449360+MathPedroza@users.noreply.github.com>
2024-12-17 14:44:46 -0300

    Update App.js

--- api-frontend/src/App.js
+++ api-frontend/src/App.js
@@ -1,32 +1,41 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'; // Importe Navigate
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import './App.css';
-import AddLembrete from './pages/AddLembrete';
-import View from './pages/View';
-import Footer from './components/Footer';
-import Nav from './components/Nav';
-import ViewLembrete from './pages/ViewLembrete';
-import Home from './pages/home'; 
-import Sobre from './pages/Sobre';
+import { ToastContainer } from 'react-toastify'; // Importa o ToastContainer para as notificações
+import 'react-toastify/dist/ReactToastify.css'; // Importa os estilos do Toastify
+import './App.css'; // Estilos gerais da aplicação
+import AddLembrete from './pages/AddLembrete'; // Página de adicionar/editar lembrete
+import View from './pages/View'; // Página para visualizar lembretes
+import Footer from './components/Footer'; // Componente do rodapé
+import Nav from './components/Nav'; // Componente de navegação
+import ViewLembrete from './pages/ViewLembrete'; // Página de visualização de lembretes
+import Home from './pages/home'; // Página inicial
+import Sobre from './pages/Sobre'; // Página "Sobre" 
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Nav/>
+        {/* Barra de navegação renderizada em todas as páginas */}
+        <Nav />
+
+        {/* Componente de Toastify para exibição de notificações */}
         <ToastContainer position="top-center" />
+
         <Routes>
-          <Route path="/" element={<Navigate to="/home" />} /> {/* Aplicação inicia já na página Home */}
-          <Route exact path="/home" element={<Home />} />
-          <Route exact path="/sobre" element={<Sobre />} />
+          {/* Redireciona automaticamente para a página Home */}
+          <Route path="/" element={<Navigate to="/home" />} /> 
+
+          {/* Definição das rotas da aplicação */}
+          <Route path="/home" element={<Home />} /> {/* Renderiza o componente Home quando a URL for /home. */}
+          <Route path="/sobre" element={<Sobre />} />
           <Route path="/addLembrete" element={<AddLembrete />} />
           <Route path="/update/:id" element={<AddLembrete />} />
           <Route path="/ViewLembrete" element={<ViewLembrete />} />
           <Route path="/view/:id" element={<View />} />
         </Routes>
       </div>
-      <Footer/>
+
+      {/* Rodapé renderizado em todas as páginas */}
+      <Footer />
     </Router>
   );
 }
